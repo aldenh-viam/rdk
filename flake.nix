558d2932784ad777ba6c48f3c1b72a60ec5187eb
Alden Hsu <alden.hsu@viam.com>
2026-02-23 10:45:05 -0500

    add nix build; update flake

--- flake.nix
+++ flake.nix
@@ -57,6 +57,86 @@
           };
         }
       );
+
+      packages = forEachSupportedSystem (
+        { pkgs }:
+        let 
+            vendorHash = "sha256-psxSxPj+7BuYvhzYArCx3RycYClz2BPA/TPMA7K7keI=";
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "rdk";
+            version = "dynamic";
+            meta.mainProgram = "server";
+            src = ./.;
+            vendorHash = vendorHash;
+
+            doCheck = false;
+            env.CGO_ENABLED = 1;
+            subPackages = [ "web/cmd/server" ];
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
+            buildInputs = with pkgs; [
+              x264
+              nlopt
+            ];
+            ldflags = [ "-s" "-w" ];
+          };
+          semi-static = pkgs.buildGoModule {
+            pname = "rdk";
+            version = "semi-static";
+            meta.mainProgram = "server";
+            src = ./.;
+            vendorHash = vendorHash;
+
+            doCheck = false;
+            env.CGO_ENABLED = 1;
+            subPackages = [ "web/cmd/server" ];
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              # upx
+            ];
+            buildInputs = with pkgs; [
+              pkgsStatic.x264
+              # remove workaround to build with clang instead of gcc because https://github.com/NixOS/nixpkgs/issues/177129
+              # disable tests because https://github.com/NixOS/nixpkgs/blob/6d41bc27aaf7b6a3ba6b169db3bd5d6159cfaa47/pkgs/by-name/nl/nlopt/package.nix#L29-L33
+              ((pkgsStatic.nlopt.overrideAttrs (oldAttrs: {
+                doCheck = false;
+              })).override { clangStdenv = stdenv; })
+            ];
+            ldflags = [ "-s" "-w" ];
+            NIX_LDFLAGS = if pkgs.stdenv.isDarwin
+              then "-lc++"
+              else "-lstdc++";
+            postFixup = ''
+              cp $out/bin/server $out/bin/server-fhs
+              patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 $out/bin/server-fhs
+              # upx --best --lzma $out/bin/server-fhs
+            '';
+          };
+          cli = pkgs.buildGoModule {
+            pname = "viam-cli";
+            version = "0.0.0";
+            meta.mainProgram = "viam";
+            src = ./.;
+            vendorHash = vendorHash;
+
+            doCheck = false;
+            env.CGO_ENABLED = 0;
+            subPackages = [ "cli/viam" ];
+
+            tags = [
+              "osusergo"
+              "netgo"
+              "no_cgo"
+            ];
+            ldflags = [ "-s" "-w" ];
+          };
+        }
+    );
     };
 }
 
